14e2b4661a42dc07d9022fd1244f36c19bcfc534
felipeforischi <felipecorreio77@gmail.com>
2020-03-01 08:34:48 -0300

    Inserção de usuário com POST

--- src/main/java/com/felipeforischi/workshopmongo/resources/UserResource.java
+++ src/main/java/com/felipeforischi/workshopmongo/resources/UserResource.java
@@ -1,14 +1,17 @@
 package com.felipeforischi.workshopmongo.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.felipeforischi.workshopmongo.domain.User;
 import com.felipeforischi.workshopmongo.dto.UserDTO;
@@ -22,7 +25,7 @@ public class UserResource {
 	private UserService service;
 	
 	@RequestMapping(method=RequestMethod.GET)
-	//@GetMapping
+	//@GetMapping -> pode ser usado no lugar de @RequestMapping(method=RequestMethod.GET)
 	public ResponseEntity<List<UserDTO>>  findAll(){
 		List<User> list = service.findAll();
 		List<UserDTO> listDto = list.stream().map(x-> new UserDTO(x)).collect(Collectors.toList()) ;
@@ -30,10 +33,20 @@ public class UserResource {
 	}
 	
 	@RequestMapping(value = "/{id}", method=RequestMethod.GET)
-	//@GetMapping
-	public ResponseEntity <UserDTO>  findById(@PathVariable String id){
+	//@GetMapping -> pode ser usado no lugar de @RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<UserDTO>  findById(@PathVariable String id){
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(new UserDTO(obj));
+	}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	//@PostMapping-> pode ser usado no lugar de @RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity <Void>  insert(@RequestBody UserDTO objDto){
+		User obj = service.fromDTO(objDto);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+		
 	}
 
 }
