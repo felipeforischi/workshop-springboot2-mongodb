4d78cff5db069dfd49a328c9b80641c51f2184d6
felipeforischi <felipecorreio77@gmail.com>
2020-03-01 10:20:35 -0300

    Atualização de usuário com PUT

--- src/main/java/com/felipeforischi/workshopmongo/resources/UserResource.java
+++ src/main/java/com/felipeforischi/workshopmongo/resources/UserResource.java
@@ -46,7 +46,13 @@ public class UserResource {
 		obj = service.insert(obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
-		
+	}
+	
+	@RequestMapping(value = "/{id}", method=RequestMethod.DELETE)
+	//@GetMapping -> pode ser usado no lugar de @RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<Void>  delete(@PathVariable String id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 
 }
